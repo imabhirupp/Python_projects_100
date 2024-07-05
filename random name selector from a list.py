aff4ef688f5722e31b7b00dd575e0aafde854f53
Abhirup Sinha <91724776+imabhirupp@users.noreply.github.com>
2024-07-05 13:34:30 +0530

    Create random name selector from a list.py

---
+++ random name selector from a list.py
@@ -0,0 +1,9 @@
+# You are working in a team of developers.
+# Another developer has written the code to import the names in the inputs
+# You can run the code to see what this names list looks like.
+# Then change the names in the input to see how it imports the names.
+import random
+a = len(names)
+random = random.randint(0,a-1)
+b = names[random]
+print(f"{b} is going to buy the meal today!")
