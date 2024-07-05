a281c5025c14c7709d37fdebc2422d8ed4b267be
Abhirup Sinha <91724776+imabhirupp@users.noreply.github.com>
2024-07-05 15:12:12 +0530

    Create rock paper scissor game.py

---
+++ rock paper scissor game.py
@@ -0,0 +1,53 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+import random
+print("Enter your choice - rock or paper or scissors")
+a = input()
+b = random.randint(0,2)
+c = [rock, paper, scissors]
+d = c[b]
+print(d)
+if a=="rock" and d==rock:
+    print("Draw")
+elif a=="rock" and d==scissors:
+    print("You win")
+elif a=="rock" and d==paper:
+    print("You lose")
+elif a=="paper" and d==paper:
+    print("Draw")
+elif a=="paper" and d==rock:
+    print("You Win")
+elif a=="paper" and d==scissors:
+    print("You loose")
+elif a=="scissors" and d==scissors:
+    print("Draw")
+elif a=="scissors" and d==rock:
+    print("You loose")
+elif a=="scissors" and d==paper:
+    print("You win")
