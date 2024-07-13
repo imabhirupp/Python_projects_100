a3a04ce400305e47525efa27633cda503ee731fe
Abhirup Sinha <91724776+imabhirupp@users.noreply.github.com>
2024-07-13 13:48:57 +0530

    Create bidding_game.py

---
+++ bidding_game.py
@@ -0,0 +1,28 @@
+#HINT: You can call clear() to clear the output in the console.
+from replit import clear
+from art import logo
+print(logo)
+
+bids ={}
+bidding_finished = False
+def highest_bid(bidding_record):
+  highest_bid= 0
+  winner = ""
+  for bidder in bidding_record:
+    bid_amount = bidding_record[bidder]
+    if bid_amount>highest_bid:
+      highest_bid=bid_amount
+      winner=bidder
+  print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+while not bidding_finished:
+  name = input("What is your name?: ")
+  price = int(input("What is your bid?: $"))
+  bids[name]=price
+  should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+  if should_continue == "no":
+    bidding_finished = True
+    highest_bid(bids)
+  elif should_continue == "yes":
+    clear()
+  
