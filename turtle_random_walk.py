8f30920224091f3534fe6081700c6f679e13da4a
Abhirup Sinha <91724776+imabhirupp@users.noreply.github.com>
2024-07-31 21:07:43 +0530

    Update turtle_random_walk.py

--- turtle_random_walk.py
+++ turtle_random_walk.py
@@ -3,6 +3,8 @@ import random
 
 tim = t.Turtle()
 
+#Using defined colors
+
 colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 directions = [0, 90, 180, 270]
 tim.pensize(15)
@@ -10,5 +12,25 @@ tim.speed("fastest")
 
 for _ in range(200):
     tim.color(random.choice(colours))
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
+
+
+#Using random colors (CREATED USING RGB)
+
+t.colormode(255)
+def radom_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    random_color(r,g,b)
+    return random_color
+
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
+
+for _ in range(200):
+    tim.color(random_color)
     tim.forward(30)
     tim.setheading(random.choice(directions))
